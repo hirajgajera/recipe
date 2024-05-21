89f3f44c9b9136d0e453851191896b563e28acda
hiraj <gajerahiraj@gmail.com>
2024-05-21 12:45:06 +0530

    garbage removal

--- fendx/src/types/navigation.ts
+++
@@ -1,4 +0,0 @@
-export type RootStackParamList = {
-  Home: undefined;
-  RecipeForm: undefined | {id: string};
-};
--- fendx/src/types/recipe.ts
+++
@@ -1,7 +0,0 @@
-export type RecipeType = {
-  id: string;
-  title: string;
-  ingredients: string[];
-  instructions: string;
-  author: string;
-};
